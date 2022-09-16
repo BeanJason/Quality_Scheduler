6fa642f7054e24dee08935568648fb536a27fd06
Jason Bean <jasonb0820@gmail.com>
2022-09-16 09:08:27 -0400

    Started sign in screen

--- QualityScheduler/App.js
+++ QualityScheduler/App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Saf } from 'react-native';
+
+//screens
+import SignInScreen from './src/screens/SignInScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.root}>
+      <SignInScreen/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
