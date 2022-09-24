5307f60f2439304f482c663f45ab43e99435f4d2
Jason Bean <jasonb0820@gmail.com>
2022-09-23 23:01:51 -0400

    Setup firebase and began work on user authentication

--- QualityScheduler/App.js
+++ QualityScheduler/App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Saf } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 //screens
 import SignInScreen from './src/screens/SignInScreen';
+import HomeScreen from './src/screens/HomeScreen';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.root}>
-      <SignInScreen/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={SignInScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ QualityScheduler/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAweH4IMO2SdMb25EYF08JYd0fBK_oUfBo",
+  authDomain: "quality-scheduler-dd348.firebaseapp.com",
+  projectId: "quality-scheduler-dd348",
+  storageBucket: "quality-scheduler-dd348.appspot.com",
+  messagingSenderId: "397032213041",
+  appId: "1:397032213041:web:cb7441464a265f55aca310",
+  measurementId: "G-D5PH3P8C42"
+};
+
+// Initialize Firebase
+let app;
+if(firebase.apps.length === 0){
+    app = firebase.initializeApp(firebaseConfig);
+}else {
+    app = firebase.app()
+}
+
+const auth = firebase.auth()
+export { auth };
